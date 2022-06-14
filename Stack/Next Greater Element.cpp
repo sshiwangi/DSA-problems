c6d7ec54734efc7e8c440cfb712364c7c4de0174
Shiwangi Kumari <sshiwangi770@gmail.com>
2022-06-14 09:08:02 +0530

    Next Greater Element

---
+++ Stack/Next Greater Element.cpp
@@ -0,0 +1,51 @@
+/*
+Given an array arr[ ] of size N having distinct elements, the task is to find the next greater element for each element of the array in order of their appearance in the array.
+Next greater element of an element in the array is the nearest element on the right which is greater than the current element.
+If there does not exist next greater of current element, then next greater element for current element is -1. For example, next greater of the last element is always -1.
+
+Example 1:
+
+Input: 
+N = 4, arr[] = [1 3 2 4]
+Output:
+3 4 4 -1
+Explanation:
+In the array, the next larger element 
+to 1 is 3 , 3 is 4 , 2 is 4 and for 4 ? 
+since it doesn't exist, it is -1. */
+
+//code
+
+class Solution
+{
+  public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+        vector<long long int> v;
+        stack<long long int> s;
+        // push the right array of an element in the stack and check the following.
+        for(int i = n-1; i>=0; i--){
+            if(s.empty()){
+                v.push_back(-1);
+            }
+            else if(!s.empty() && s.top()>arr[i]){
+                v.push_back(s.top());
+            }
+            else if(!s.empty() && s.top()<=arr[i]){
+                while(!s.empty() && s.top()<=arr[i]){
+                    s.pop();
+                }
+                if(s.empty()){
+                    v.push_back(-1);
+                }
+                else{
+                    v.push_back(s.top());
+                }
+            }
+            s.push(arr[i]);
+        }
+        reverse(v.begin(),v.end());
+        return v;
+    }
+};
