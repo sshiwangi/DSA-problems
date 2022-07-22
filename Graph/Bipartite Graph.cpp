33c3027c080ce3350ab24fa245dd9e80b8d08cd6
Shiwangi Kumari <sshiwangi770@gmail.com>
2022-07-22 12:25:36 +0530

    updating bipartite graph code

--- Graph/Bipartite Graph.cpp
+++ Graph/Bipartite Graph.cpp
@@ -7,24 +7,30 @@ class Solution
 {
 public:
     // Function to check if the graph is bipartite or not
-    bool checkBipartite(int node, vector<int>adj[], vector<int> &color){
+    bool checkBipartite(int src, vector<int>adj[], int color[]){
+        queue<int>q;
+        q.push(src);
         // if node not colored, color it and check for it's adjacent
-        if(color[node] == -1) color[node] = 1;
-        for(auto it: adj[node]){
-            if(color[it] == -1){
-                color[it] = 1 - color[node];
-                if(!checkBipartite(it,adj,color)){
-                    return false;
+        color[src] = 1;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(auto it: adj[node]){
+                if(color[it] == -1){
+                   color[it] = 1 - color[node];
+                   q.push(it);
                 }
                 // if colored, then check if it doesn't have same color as it's adjacent
                 else if(color[it] == color[node]) return false;
             }
+        
         }
+        
         return true;
     }
 	bool isBipartite(int V, vector<int>adj[]){
 	    // Code here
-	    int color[n];
+	    int color[V];
 	    memset(color,-1,sizeof color);
 	    for(int i = 0; i<V; i++){
 	        if(color[i] == -1){ // if the node is not colored
