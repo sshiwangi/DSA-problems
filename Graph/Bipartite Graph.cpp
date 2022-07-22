e8affce4a630930d7a5fc372078a94d01384ac78
Shiwangi Kumari <sshiwangi770@gmail.com>
2022-07-22 10:17:34 +0530

    Bipartite graph

---
+++ Graph/Bipartite Graph.cpp
@@ -0,0 +1,39 @@
+/* Given an adjacency list of a graph adj  of V no. of vertices having 0 based index. Check whether the graph is bipartite or not.
+Expected Time Complexity: O(V + E)
+Expected Space Complexity: O(V) */
+
+//code
+class Solution
+{
+public:
+    // Function to check if the graph is bipartite or not
+    bool checkBipartite(int node, vector<int>adj[], vector<int> &color){
+        // if node not colored, color it and check for it's adjacent
+        if(color[node] == -1) color[node] = 1;
+        for(auto it: adj[node]){
+            if(color[it] == -1){
+                color[it] = 1 - color[node];
+                if(!checkBipartite(it,adj,color)){
+                    return false;
+                }
+                // if colored, then check if it doesn't have same color as it's adjacent
+                else if(color[it] == color[node]) return false;
+            }
+        }
+        return true;
+    }
+	bool isBipartite(int V, vector<int>adj[]){
+	    // Code here
+	    int color[n];
+	    memset(color,-1,sizeof color);
+	    for(int i = 0; i<V; i++){
+	        if(color[i] == -1){ // if the node is not colored
+	            if(!checkBipartite(i,adj,color)){
+	               return false; 
+	            }
+	        } 
+	    }
+	    return true;
+	}
+
+};
